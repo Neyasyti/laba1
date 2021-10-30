b73d0d5f71125fc6f3b00fa4978a84bd31613829
Neyasyti <alexla5fn@gmail.com>
2021-10-30 14:21:10 +0300

    rt

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace laba1
     {
         static void Main(string[] args)
         {
+            
+            wr();
+            
+            
+
+        }
+        static void wr()
+        {
+            int a;
+            a = 6;
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hell0 World!");
+            Console.WriteLine(rt(a));
+        }
+        static int rt(int a)
+        {
+            a = a + 3;
+            return (a);
         }
     }
+
 }
